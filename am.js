3b6c34c0c7d7c831adefcd298dbdcf97c2e295a3
Shraddha34 <shivanidabde2000@gmail.com>
2024-07-31 15:12:06 +0530

    Add files via upload

---
+++ am.js
@@ -0,0 +1,47 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+   
+  function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+    xhttp.open("GET", "ajax_info.txt");
+    xhttp.send();
+  }
+  $('#play-video').on('click', function(e){
+    e.preventDefault();
+    $('#video-overlay').addClass('open');
+    $("#video-overlay").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
+  });
+  
+  $('.video-overlay, .video-overlay-close').on('click', function(e){
+    e.preventDefault();
+    close_video();
+  });
+  
+  $(document).keyup(function(e){
+    if(e.keyCode === 27) { close_video(); }
+  });
+  
+  function close_video() {
+    $('.video-overlay.open').removeClass('open').find('iframe').remove();
+  };
